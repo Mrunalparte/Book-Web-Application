5337d341ab9489c52ff645e741c9d5c718806dab
Mrunalparte <89187936+Mrunalparte@users.noreply.github.com>
2024-01-23 17:16:50 +0530

    Add files via upload

---
+++ EditServlet.java
@@ -0,0 +1,62 @@
+package com.jsp.bsa.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/editurl")
+public class EditServlet extends HttpServlet {
+	private static final String query = "update BOOKDATA set BOOKNAME=?,BOOKEDITION=?,BOOKPRICE=? where id=?";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		resp.setContentType("text/html");
+		
+		int id = Integer.parseInt(req.getParameter("id"));
+		
+		String bookName = req.getParameter("bookName");
+		String bookEdition = req.getParameter("bookEdition");
+		float bookPrice = Float.parseFloat(req.getParameter("bookPrice"));
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try(Connection con = DriverManager.getConnection("jdbc:mysql:///book", "root", "mrunal@16120");
+				PreparedStatement ps = con.prepareStatement(query);){
+			ps.setString(1, bookName);
+			ps.setString(2, bookEdition);
+			ps.setFloat(3, bookPrice);
+			ps.setInt(4, id);
+			int count = ps.executeUpdate();
+			if (count == 1) {
+				pw.println("<h2>Record is Edited Successfully</h2>");
+			} else {
+				pw.println("<h2>Record is not Edited Successfully</h2>");
+			}
+	}catch (SQLException se) {
+		se.printStackTrace();
+		pw.println("<h1>"+se.getMessage()+"</h2>");
+		
+	}catch (Exception e) {
+		e.printStackTrace();
+		pw.println("<h1>"+e.getMessage()+"</h2>");
+	}
+		pw.println("<a href='home.html'>Home</a>");
+		pw.println("<br>");
+		pw.println("<a href='bookList'>Book List</a>");
+}		
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
